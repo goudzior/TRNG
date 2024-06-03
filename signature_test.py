18ffae9b11fd575f212cc65890f66c490ad767c2
goudzior <123596025+goudzior@users.noreply.github.com>
2024-06-03 06:08:38 +0000

    fixed minor issues

--- signature_test.py
+++ signature_test.py
@@ -32,27 +32,27 @@ def generate_rsa_key_pair():
     phi_n = (p - 1) * (q - 1)
     
     # Wybieramy liczbę e (public_exponent) względnie pierwszą z phi_n
-    e = 257
+    e = 65537
     
     # Obliczamy liczbę d (private_exponent) będącą odwrotnością modulo e względem phi_n
     d = pow(e, -1, phi_n)
     
     # Zwracamy klucze publiczny i prywatny
-    public_key = (n, e)
-    private_key = (n, d)
+    private_key = (n, e)
+    public_key = (n, d)
     
-    return public_key, private_key
+    return private_key, public_key
 
 # Funkcja szyfrująca wiadomość za pomocą klucza publicznego
-def encrypt_message(message: str, public_key: tuple) -> int:
-    n, e = public_key
+def encrypt_message(message: str, private_key: tuple) -> int:
+    n, e = private_key
     message_hash = int(hash_message(message), 16)
     encrypted_message = pow(message_hash, e, n)
     return encrypted_message
 
 # Funkcja deszyfrująca wiadomość za pomocą klucza prywatnego
-def decrypt_message(encrypted_message: int, private_key: tuple) -> str:
-    n, d = private_key
+def decrypt_message(encrypted_message: int, public_key: tuple) -> str:
+    n, d = public_key
     decrypted_message_hash = pow(encrypted_message, d, n)
     decrypted_message_hash_hex = hex(decrypted_message_hash)[2:]
     return decrypted_message_hash_hex.zfill(64)
@@ -60,51 +60,54 @@ def decrypt_message(encrypted_message: int, private_key: tuple) -> str:
 # Główna funkcja
 def main():
     message = "test message"
+    print (f"Wiadomość 1: {message}")
     message_hash = hash_message(message)
     print(f"Skrót wiadomości: {message_hash}")
 
-    public_key, private_key = generate_rsa_key_pair()
+    private_key, public_key = generate_rsa_key_pair()
     print(f"Klucz publiczny: {public_key}")
     print(f"Klucz prywatny: {private_key}")
 
-    encrypted_message = encrypt_message(message, public_key)
+    encrypted_message = encrypt_message(message, private_key)
     print(f"Zaszyfrowany skrót wiadomości: {encrypted_message}")
 
-    decrypted_message_hash = decrypt_message(encrypted_message, private_key)
+    decrypted_message_hash = decrypt_message(encrypted_message, public_key)
     print(f"Odszyfrowany skrót wiadomości: {decrypted_message_hash}")
 
     # Sprawdzamy, czy odszyfrowany skrót jest taki sam jak oryginalny skrót
     if decrypted_message_hash == message_hash:
-        print("Skrót wiadomości został poprawnie odszyfrowany.")
+        print("Oba skróty są takie same.")
     else:
-        print("Skrót wiadomości NIE został poprawnie odszyfrowany.")
+        print("Oba skróty NIE są takie same.")
 
     print ("------TEST 1: próba odszyfrowania innym kluczem------")
-    public_key2, private_key2 = generate_rsa_key_pair()
-    decrypted_message_hash_diff_key = decrypt_message(encrypted_message, private_key2)
+    private_key2, public_key2 = generate_rsa_key_pair()
+    print(f"Klucz publiczny: {public_key2}")
+    print(f"Klucz prywatny: {private_key2}")
+    decrypted_message_hash_diff_key = decrypt_message(encrypted_message, public_key2)
     print(f"Odszyfrowany skrót wiadomości: {decrypted_message_hash_diff_key}")
 
     if decrypted_message_hash_diff_key == message_hash:
-        print("Skrót wiadomości został poprawnie odszyfrowany.")
+        print("Oba skróty są takie same.")
     else:
-        print("Skrót wiadomości NIE został poprawnie odszyfrowany.")
+        print("Oba skróty NIE są takie same.")
 
     print ("------TEST 2: Test integralności------")
     message2 = "test message123"
+    print (f"Wiadomość 2: {message2}")
     message_hash2 = hash_message(message2)
     print(f"Skrót wiadomości: {message_hash2}")
 
-    encrypted_message2 = encrypt_message(message2, public_key2)
+    encrypted_message2 = encrypt_message(message2, private_key2)
     print(f"Zaszyfrowany skrót wiadomości: {encrypted_message2}")
 
-    decrypted_message_hash2 = decrypt_message(encrypted_message2, private_key2)
+    decrypted_message_hash2 = decrypt_message(encrypted_message2, public_key2)
     print(f"Odszyfrowany skrót wiadomości: {decrypted_message_hash2}")
 
     if decrypted_message_hash2 == decrypted_message_hash:
-        print("Skrót wiadomości został poprawnie odszyfrowany.")
+        print("Oba skróty są takie same.")
     else:
-        print("Skrót wiadomości NIE został poprawnie odszyfrowany.")
-
+        print("Oba skróty NIE są takie same.")
     
 
 if __name__ == "__main__":
